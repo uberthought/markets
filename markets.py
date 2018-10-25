7f7f36c70a23128fe6948be46876e3a75c7920ce
Philip Richardson <uberthought@gmail.com>
2018-10-25 17:35:09 -0400

    split into training and testing datasets

--- markets.py
+++ markets.py
@@ -22,16 +22,26 @@ def gen():
 
     yield input, label
 
+length = len(data)
 dataset = tf.data.Dataset.from_generator(gen, (tf.float32, tf.float32), (tf.TensorShape([5]), tf.TensorShape([1])))
-dataset = dataset.shuffle(len(data))
-testing_dataset = dataset.take(64)
-training_dataset = dataset.skip(64)
+dataset = dataset.shuffle(length)
+testing_length = int(length * 0.1)
+
+testing_dataset = dataset.take(testing_length)
+testing_dataset = testing_dataset.repeat()
+testing_dataset = testing_dataset.batch(testing_length)
+
+training_dataset = dataset.skip(testing_length)
 training_dataset = training_dataset.repeat()
 training_dataset = training_dataset.batch(32)
 
+handle = tf.placeholder(tf.string, shape=[])
+iterator = tf.data.Iterator.from_string_handle(handle, training_dataset.output_types, training_dataset.output_shapes)
 
-training_iter = training_dataset.make_one_shot_iterator()
-input, label = training_iter.get_next()
+training_iterator = training_dataset.make_one_shot_iterator()
+testing_iterator = testing_dataset.make_one_shot_iterator()
+
+input, label = iterator.get_next()
 
 hidden1 = tf.layers.dense(inputs=input, units=5, activation=tf.tanh)
 concat1 = tf.concat([input, hidden1], 1)
@@ -46,11 +56,16 @@ diff = tf.subtract(label, output)
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
+training_handle = sess.run(training_iterator.string_handle())
+testing_handle = sess.run(testing_iterator.string_handle())
+
 for i in range(100000):
-  train_step.run()
+  train_step.run(feed_dict={handle: training_handle})
 
   if i % 1000 == 0:
-    training_loss = loss.eval()
-    print("Step %d, training batch loss %f"%(i, training_loss))
+    training_loss = loss.eval(feed_dict={handle: training_handle})
+    testing_loss = loss.eval(feed_dict={handle: testing_handle})
+    print("Step %d, training loss: %f testing loss: %f"%(i, training_loss, testing_loss))
 
-print(loss.eval())
+print(loss.eval(feed_dict={handle: training_handle}))
+print(loss.eval(feed_dict={handle: testing_handle}))
