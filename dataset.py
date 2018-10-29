f03153969313cfaacbb693a7eb7b1c8c65bee2bb
Philip Richardson <uberthought@gmail.com>
2018-10-29 16:54:09 -0400

    not sure

--- dataset.py
+++ dataset.py
@@ -9,22 +9,16 @@ def create(name):
     with open(fileName,'rb') as file:
         data = pickle.load(file)
 
+    data_list = list(reversed(list(data.items())))
+
     # names = np.repeat(name, len(data))
-    # dates = [datetime.strptime(k[0], '%Y-%m-%d').timestamp() for k in data.items()]
-    opens = [float(k[1]['1. open']) for k in data.items()]
-    highs = [float(k[1]['2. high']) for k in data.items()]
-    lows = [float(k[1]['3. low']) for k in data.items()]
-    closes = [float(k[1]['4. close']) for k in data.items()]
+    # dates = [datetime.strptime(k[0], '%Y-%m-%d').timestamp() for k in data_list]
+    opens = [float(k[1]['1. open']) for k in data_list]
+    highs = [float(k[1]['2. high']) for k in data_list]
+    lows = [float(k[1]['3. low']) for k in data_list]
+    closes = [float(k[1]['4. close']) for k in data_list]
 
     return np.stack((opens, highs, lows, closes), axis=1)
-    # max = np.max(foo)
-    # min = np.min(foo)
-    # foo = (foo - min) / (max - min)
-    # first = foo[0][0]
-    # foo = foo - first
-
-    # return foo
-    # return tf.data.Dataset.from_tensor_slices(foo)
 
 # if __name__ == "__main__":
 #     dataset = create('GOOGL')
--- markets.py
+++ markets.py
@@ -1,39 +1,52 @@
 import tensorflow as tf
+import numpy as np
 from dataset import create
 
-data = create('GOOGL')
+googl = create('GOOGL')
+msft = create('MSFT')
+aapl = create('AAPL')
+
+# data = np.concatenate((aapl, googl, msft))
+data = googl
+
+feed_size = 10
 
 print(len(data))
 
 def gen():
-  size = 6
+  size = feed_size + 1
   for i in range(len(data) - size):
     window = data[i:i + size]
     
     input = window[:,0]
-    input = [10 * (input[i] - input[i+1]) / input[i] for i in range(len(input) - 1)]
+    input = [(input[i+1] - input[i]) / input[i] for i in range(len(input) - 1)]
 
-    # label = [window[size - 1][3]]
     last = window[size - 1]
-    if last[0] < last[3]:
+    last_open = last[0]
+    last_close = last[3]
+    change = (last_close - last_open) / last_open
+    if change > 0:
       label = [1]
     else:
       label = [0]
 
-    yield input, label
+    yield input, label, [last_open, last_close, change]
 
 length = len(data)
-dataset = tf.data.Dataset.from_generator(gen, (tf.float32, tf.float32), (tf.TensorShape([5]), tf.TensorShape([1])))
-dataset = dataset.shuffle(length)
+dataset = tf.data.Dataset.from_generator(gen, (tf.float32, tf.float32, tf.float32), (tf.TensorShape([feed_size]), tf.TensorShape([1]), tf.TensorShape([3])))
+# dataset = dataset.shuffle(length)
 testing_length = int(length * 0.1)
+# testing_length = 20
+training_length = length - testing_length
 
-testing_dataset = dataset.take(testing_length)
-testing_dataset = testing_dataset.repeat()
-testing_dataset = testing_dataset.batch(testing_length)
-
-training_dataset = dataset.skip(testing_length)
+training_dataset = dataset.take(training_length)
+training_dataset = training_dataset.shuffle(training_length)
 training_dataset = training_dataset.repeat()
 training_dataset = training_dataset.batch(32)
+
+testing_dataset = dataset.skip(training_length)
+testing_dataset = testing_dataset.repeat()
+testing_dataset = testing_dataset.batch(testing_length)
 
 handle = tf.placeholder(tf.string, shape=[])
 iterator = tf.data.Iterator.from_string_handle(handle, training_dataset.output_types, training_dataset.output_shapes)
@@ -41,17 +54,21 @@ iterator = tf.data.Iterator.from_string_handle(handle, training_dataset.output_t
 training_iterator = training_dataset.make_one_shot_iterator()
 testing_iterator = testing_dataset.make_one_shot_iterator()
 
-input, label = iterator.get_next()
+input, label, meta = iterator.get_next()
 
-hidden1 = tf.layers.dense(inputs=input, units=5, activation=tf.tanh)
-concat1 = tf.concat([input, hidden1], 1)
-hidden2 = tf.layers.dense(inputs=concat1, units=5, activation=tf.tanh)
-concat2 = tf.concat([concat1, hidden2], 1)
-output = tf.layers.dense(inputs=concat2, units=1, activation=tf.sigmoid)
+hidden_width = 10
+hidden_depth = 10
+
+hidden = input
+for i in range(hidden_depth):
+  dense = tf.layers.dense(inputs=hidden, units=hidden_width, activation=tf.tanh)
+  concat = tf.concat([dense, hidden], 1)
+  hidden = concat
+
+output = tf.layers.dense(inputs=hidden, units=1, activation=tf.nn.sigmoid)
 
 loss = tf.losses.mean_squared_error(label, output)
-train_step = tf.train.AdagradOptimizer(1e-1).minimize(loss)
-diff = tf.subtract(label, output)
+train_step = tf.train.AdagradOptimizer(1e-2).minimize(loss)
  
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
@@ -64,8 +81,24 @@ for i in range(100000):
 
   if i % 1000 == 0:
     training_loss = loss.eval(feed_dict={handle: training_handle})
-    testing_loss = loss.eval(feed_dict={handle: testing_handle})
-    print("Step %d, training loss: %f testing loss: %f"%(i, training_loss, testing_loss))
+    testing_loss, b, c, d = sess.run([loss, label, output, meta], feed_dict={handle: testing_handle})
+    e = np.concatenate((b, c, d), axis=1)
 
-print(loss.eval(feed_dict={handle: training_handle}))
-print(loss.eval(feed_dict={handle: testing_handle}))
+    u = 0
+    v = 0
+    w = 0
+    for [label0, output0, open0, close0, change0] in e:
+      u = u + change0
+
+      if output0 > 0.5:
+        v = v + change0
+
+      if label0 > 0.5:
+        w = w + change0
+
+    u = u * 100
+    v = v * 100
+    w = w * 100
+    d = v - u
+
+    print("Step %d, training loss: %f testing loss: %f yield: %f ours: %f best: %f difference: %f"%(i, training_loss, testing_loss, u, v, w, d))
